c11c8bc02b4675bb570fdee4d15709a7fc43910f
Maulik Nariya <mauliknariya@gmail.com>
2022-03-30 21:38:51 -0400

    added , returns baseline data for all genes, or specified gene-list, or train-test split for RFE

--- src/datasets.py
+++ src/datasets.py
@@ -1,8 +1,52 @@
 import pandas as pd
+from options_parser import arguments
+
+options, args = arguments()
+
+def brca_data(train_test=False, full_data=False):
+    # input cells over which the predsictive model is built
+    input_cells = open(options.cell_list, "r").read().splitlines()
+    input_cells = [ic.replace("-", "").upper() for ic in input_cells]
+
+    # baseline data
+    dfbase = pd.read_csv(options.baseline_data, index_col=0)
+    base_cells = dfbase.columns.tolist()
+
+    # list of genes (or proteins) which would serve as the features for predictive modeling
+    genes = open(options.gene_list, "r").read().splitlines()
+
+    # GR-metrics data
+    dfgr = pd.read_csv(options.response_data)
+    dfgr = dfgr[dfgr.generic_name==options.drug]
+    dfgr.index=dfgr.cell_line
+    gr_cells = dfgr.index.tolist()
+
+    # cells that are present in both, baseline as well GR-data
+    cells = list(set(input_cells).intersection(set(base_cells).intersection(set(gr_cells))))
+    dfbase = dfbase[cells]
+    dfgr = dfgr[dfgr.index.isin(cells)]
+
+    # combine the baseline and dose response data into one dataFrame
+    dfc = pd.concat([dfbase[dfbase.index.isin(genes)].T,
+                    dfgr[[options.metric, "sigma_%s"%options.metric]]], axis=1).dropna()
+    dff = pd.concat([dfbase.T,
+                    dfgr[[options.metric, "sigma_%s"%options.metric]]], axis=1).dropna()
+
+    # train-test sets, with roughly 80-20 split, for rescursive feature elimination
+    dftest = pd.read_csv(options.test_set, index_col=0)
+    test_cells = dftest[dftest["test"]==1].index.tolist()
+    train_cells = list(set(cells)-set(test_cells))
+    if train_test is False:
+        if full_data is False:
+            return dfc
+        else:
+            return dff
+    else:
+        return train_cells, test_cells
 
 def load_response(gr_values=False):
     dfgr = pd.read_csv("../data/grmetrics.csv")
-    dfgrvals = pd.read_csv("../data/grvalues.csv", index_col=0)
+    dfgrvals = pd.read_csv("../data/grvalues.csv")
     if gr_values is False: 
         return dfgr
     else:
@@ -18,3 +62,4 @@ def load_meta():
     dfcells = pd.read_csv("../data/cell_lines_metadata.csv")
     dfdrugs = pd.read_csv("../data/agents_metadata.csv")
     return dfcells, dfdrugs
+
