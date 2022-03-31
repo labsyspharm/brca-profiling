d3b8ce6e0126f766d350c7a65b3ea17bfb33a6d2
Artem Sokolov <artem.sokolov@gmail.com>
2022-03-31 13:06:20 -0400

    Update (#22)
    * Added whitespace (#19)
    
    * updated generic names of some drugs in
    
    * removed , option parser in separate module
    
    * test set (~20%) used in recursive feature elimination
    
    * added , returns baseline data for all genes, or specified gene-list, or train-test split for RFE
    
    * option parser module
    
    * recursive feature elimination
    
    * support vector regression
    
    * ridge regression
    
    * removed feature_prune, options in separate module
    
    * added brca_data, returns baseline data for all genes, specified gene-list or train-test split for RFE
    
    * includes column 'generic_name'
    
    * includes column 'generic_name'
    
    * Handle 0 pairs
    
    * Updated senexin-B in the automated test
    
    * Added back the log messages
    
    Co-authored-by: Maulik Nariya <mauliknariya@gmail.com>

--- src/datasets.py
+++ src/datasets.py
@@ -1,8 +1,58 @@
 import pandas as pd
+from options_parser import arguments
+
+options, args = arguments()
+
+def brca_data(train_test=False, full_data=False):
+    # input cells over which the predsictive model is built
+    input_cells = open(options.cell_list, "r").read().splitlines()
+    input_cells = [ic.replace("-", "").upper() for ic in input_cells]
+    print("Parsed %s user-provided cell line names"%len(input_cells))
+
+    # baseline data
+    dfbase = pd.read_csv(options.baseline_data, index_col=0)
+    base_cells = dfbase.columns.tolist()
+    msg = "Loaded %s containing %s features and %s cell lines"
+    print(msg%(options.baseline_data, dfbase.shape[0], dfbase.shape[1]))
+
+    # list of genes (or proteins) which would serve as the features for predictive modeling
+    genes = open(options.gene_list, "r").read().splitlines()
+    print("Parsed %s user-provided gene names"%len(genes))
+
+    # GR-metrics data
+    dfgr = pd.read_csv(options.response_data)
+    dfgr = dfgr[dfgr.generic_name==options.drug]
+    dfgr.index=dfgr.cell_line
+    gr_cells = dfgr.index.tolist()
+    print("Loaded GR values for %s cell lines"%len(gr_cells))
+
+    # cells that are present in both, baseline as well GR-data
+    cells = list(set(input_cells).intersection(set(base_cells).intersection(set(gr_cells))))
+    dfbase = dfbase[cells]
+    dfgr = dfgr[dfgr.index.isin(cells)]
+    print("%s cell lines are in common to all sources"%len(cells))
+
+    # combine the baseline and dose response data into one dataFrame
+    dfc = pd.concat([dfbase[dfbase.index.isin(genes)].T,
+                    dfgr[[options.metric, "sigma_%s"%options.metric]]], axis=1).dropna()
+    dff = pd.concat([dfbase.T,
+                    dfgr[[options.metric, "sigma_%s"%options.metric]]], axis=1).dropna()
+
+    # train-test sets, with roughly 80-20 split, for rescursive feature elimination
+    dftest = pd.read_csv(options.test_set, index_col=0)
+    test_cells = dftest[dftest["test"]==1].index.tolist()
+    train_cells = list(set(cells)-set(test_cells))
+    if train_test is False:
+        if full_data is False:
+            return dfc
+        else:
+            return dff
+    else:
+        return train_cells, test_cells
 
 def load_response(gr_values=False):
     dfgr = pd.read_csv("../data/grmetrics.csv")
-    dfgrvals = pd.read_csv("../data/grvalues.csv", index_col=0)
+    dfgrvals = pd.read_csv("../data/grvalues.csv")
     if gr_values is False: 
         return dfgr
     else:
--- src/random_forest.py
+++ src/random_forest.py
@@ -1,48 +1,21 @@
 import os
 import subprocess
+import warnings
 import pandas as pd
 import numpy as np
-from optparse import OptionParser
-import warnings
 from sklearn.ensemble import RandomForestRegressor
 from scipy.stats import spearmanr
+from warnings import filterwarnings
 from lpocv.lpocv import LeavePairOut
-import os
-import subprocess
+from options_parser import arguments
+from datasets import brca_data
 
-brca_dir = os.path.dirname(os.path.dirname(__file__))
-data_dir = os.path.join(brca_dir, "data")
-results_dir = os.path.join(os.getcwd(), "brca_results")
-if os.path.isdir(results_dir):
-    pass
-else:
-    subprocess.run(["mkdir", results_dir])
 
-# Option parser
-###############
-parser = OptionParser()
-parser.add_option("-b", "--baseline-file",  dest="baseline_data", type="str", default="%s/rnaseq_log2rpkm.csv"%data_dir,
-                  help="baseline data - RNAseq, mass-spectometry, phospho mass-spectometry, kinase activity")
-parser.add_option("-r", "--response-file", dest="response_data", type="str", default="%s/grmetrics.csv"%data_dir,
-                  help="dose-response for drug-cell combinations, columns headers - 'GR_AOC', 'sigma_GR_AOC', 'GRmax', 'sigma_GR_AOC', 'GEC', 'sigma_GEC50'")
-parser.add_option("-g", "--gene-list", dest="gene_list", type="str", default="%s/gene_list.txt"%data_dir,
-                  help="subset of genes on which the predictions are made")
-parser.add_option("-c", "--cell-list", dest="cell_list", type="str", default="%s/cell_list.txt"%data_dir,
-                  help="subset of genes on which the predictions are made")
-parser.add_option("-d", "--drug", dest="drug", type="str", default="Abemaciclib/LY2835219", help="drug name")
-parser.add_option("-m", "--metric", dest="metric", type="str", default="GR_AOC",
-                  help="metric to be predicted, possible values: GR_AOC, GRmax, GEC50")
-parser.add_option("-p", "--parameters", dest="params", type="str", default="%s/randomforest_params.txt"%data_dir,
-                  help="file containing the parameters for RandomForestRegressor")
-parser.add_option("-t", "--prediction_type", dest="prediction_type", type="str", default="predict_genes",
-                  help="switch between evaluating feature importance and estimating model accuracy - predict_genes, estimate_accuracy")
-parser.add_option("-n", "--num_pairs", dest="complexity", type="int", default=5,
-                  help="Computational complexity, if None then AUC is evaluated over all possible cell pairs, by default each cell is paired with 5 other cells.")
-parser.add_option("-f", "--num_features", dest="num_features", type="int", default=50,
-                  help="Number of features for feature pruning")
-parser.add_option("-o", "--output", dest="output", type="str", default=results_dir,
-                  help="output folder")
-options, args = parser.parse_args()
+# OptionParser
+options, args = arguments()
+
+# Breast cancer data
+dfc = brca_data()
 
 # Random forest parameters
 ##########################
@@ -51,37 +24,47 @@ param_name = dfparam["parameter"].tolist()
 value = dfparam["value"].tolist()
 for i in range(dfparam.shape[0]): exec("%s=%d"%(param_name[i], value[i]))
 
-# Data
-########
-input_cells = open(options.cell_list, "r").read().splitlines()
-input_cells = [ic.replace("-", "").upper() for ic in input_cells]
-print("Parsed %s user-provided cell line names"%len(input_cells))
+# AUC random forest regressor
+def auc_random_forest(df):
+    '''
+    Parameters:
+    ##########
+    df: pandas dataframe, shape=(rows, columns)
+        rows: features (genes)
+        columns: samples + growth rate metrics
+        must contain a column METRIC (e.g. "GR_AOC", "GR_max", "GR50", etc.) and its coressponding "sigma_METRIC"
+    Returns:
+    ########
+    auc: float
+        Estimate of model accuracy
+    dfout: pandas dataframe
+        Measured and predicted values in each cross-validation fold.
+    '''
+    X = df.drop(columns=[options.metric, "sigma_%s"%options.metric]).values
+    y = df[options.metric].values
+    cell_line = dfc.index.tolist()
+    yerr = df["sigma_%s"%options.metric].values
+    auc=0
+    itr=0
+    dfout = pd.DataFrame()
+    for train, test in LeavePairOut().split(X, y, 2.0*yerr, num_pairs=options.complexity):
+        random_forest = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=random_state)
+        random_forest.fit(X[train], y[train])
+        ypred = random_forest.predict(X[test])
+        itr+=1
+        df0=pd.DataFrame(data=[["cv_iteration%s"%itr, cell_line[0], y[test[0]], ypred[0]]],
+                         columns=["CV-fold", "cell_line", "measured", "predicted"])
+        df1=pd.DataFrame(data=[["cv_iteration%s"%itr, cell_line[1], y[test[1]], ypred[1]]],
+                         columns=["CV-fold", "cell_line", "measured", "predicted"])
 
-dfbase = pd.read_csv(options.baseline_data, index_col=0)
-base_cells = dfbase.columns.tolist()
-msg = "Loaded %s containing %s features and %s cell lines"
-print(msg%(options.baseline_data, dfbase.shape[0], dfbase.shape[1]))
+        dfout = pd.concat([dfout, df0], ignore_index=True)
+        dfout = pd.concat([dfout, df1], ignore_index=True)
+        if (ypred[0]-ypred[1])*(y[test[0]]-y[test[1]]) > 0:
+            auc+=1
+    print("Evaluated %s pairs using leave-pair-out cross-validation."%itr)
+    auc = float(auc/itr) if itr > 0 else np.nan
+    return auc, dfout
 
-genes = open(options.gene_list, "r").read().splitlines()
-print("Parsed %s user-provided gene names"%len(genes))
-
-dfgr = pd.read_csv(options.response_data)
-dfgr = dfgr[dfgr.agent==options.drug]
-dfgr.index=dfgr.cell_line
-gr_cells = dfgr.index.tolist()
-print("Loaded GR values for %s cell lines"%len(gr_cells))
-
-cells = set(input_cells).intersection(set(base_cells).intersection(set(gr_cells)))
-print("%s cell lines are in common to all sources"%len(cells))
-
-dfbase = dfbase[list(cells)]
-dfgr = dfgr[dfgr.index.isin(cells)]
-dfc = pd.concat([dfbase[dfbase.index.isin(genes)].T,
-                 dfgr[[options.metric, "sigma_%s"%options.metric]]], axis=1).dropna()
-dff = pd.concat([dfbase.T,
-                 dfgr[[options.metric, "sigma_%s"%options.metric]]], axis=1).dropna()
-
-#####################################################################################################################################
 def feature_importance(df):
     '''
     Function to evaluate feature importance of input feature set. 
@@ -90,14 +73,14 @@ def feature_importance(df):
     df: pandas dataframe, shape=(rows,columns) 
         rows: features (genes)
         columns: samples + growth rate metrics 
-        must contain a column "METRIC" and its coressponding "sigma_METRIC"
+        must contain a column METRIC (e.g. "GR_AOC", "GR_max", "GR50", etc.) and its coressponding "sigma_METRIC"
     Returns:
     ########
     dfimp: pandas dataframe
         shape = (rows,columns) rows: features (genes)
-                               columns: feature importance, rho, pval of Spearman rank correlation between
-                               gene expression and growth rate metric across all cells
+        columns: feature importance, rho, pval of Spearman rank correlation between gene expression and growth rate metric across all cells
     '''
+    filterwarnings("ignore")
     X = df.drop(columns=[options.metric, "sigma_%s"%options.metric]).values
     y = df[options.metric].values
     random_forest = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth,
@@ -117,89 +100,20 @@ def feature_importance(df):
         dfimp.loc[feature, "spearman_pval"] = pval
     return dfimp
 
-
-def feature_pruning(df, num_features):
-    '''
-    Iterative feature pruning for data-driven feature selection, drops 25% of the least important features in every iteration
-    Parameters:
-    ##########
-    df: pandas dataframe, shape=(rows,columns) 
-        rows: features (genes)
-        columns: samples + growth rate metrics 
-        must contain a column "METRIC" and its coressponding "sigma_METRIC"
- 
-    num_features: int, default=50
-            number of features to be returned
-    '''
-    dfimp = feature_importance(df)
-    while dfimp.shape[0] > (options.num_features+1):
-        drop_features = dfimp.tail(int(0.25*dfimp.shape[0])).index.tolist()
-        df = df.drop(columns=drop_features)
-        dfimp = feature_importance(df)
-    return dfimp
-
-
-def compute_auc(df):
-    '''
-    Random forest regressor.
-    Parameters:
-    ##########
-    df: pandas dataframe, shape=(rows,column"""s)
-        rows: features (genes)
-        columns: samples + growth rate metrics 
-        must contain a column "METRIC" and its coressponding "sigma_METRIC"
-    Returns:
-    ########
-    auc: float
-        Estimate of model accuracy
-    dfout: pandas dataframe
-        Measured and predicted values in each cross-validation fold.
-    '''
-    X = df.drop(columns=[options.metric, "sigma_%s"%options.metric]).values
-    y = df[options.metric].values
-    cell_line = dfc.index.tolist()
-    yerr = df["sigma_%s"%options.metric].values
-    auc=0
-    itr=0
-    dfout = pd.DataFrame()
-    num_pairs=options.complexity
-    for train, test in LeavePairOut().split(X, y, 2.0*yerr, num_pairs=num_pairs):
-        random_forest = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth,
-                                              random_state=random_state)
-        random_forest.fit(X[train], y[train])
-        ypred = random_forest.predict(X[test])
-        itr+=1
-        df0=pd.DataFrame(data=[["cv_iteration%s"%itr, cell_line[0], y[test[0]], ypred[0]]],
-                         columns=["CV-fold", "cell_line", "measured", "predicted"])
-        df1=pd.DataFrame(data=[["cv_iteration%s"%itr, cell_line[1], y[test[1]], ypred[1]]],
-                         columns=["CV-fold", "cell_line", "measured", "predicted"])
-
-        dfout = dfout.append(df0, ignore_index=True)
-        dfout = dfout.append(df1, ignore_index=True)
-        if (ypred[0]-ypred[1])*(y[test[0]]-y[test[1]]) > 0:
-            auc+=1
-    print("Evaluated %s pairs using leave-pair-out cross-validation."%itr)
-    auc = float(auc/itr) if itr > 0 else np.nan
-    return auc, dfout
-
-####################################################################################################################################
+#####################################################
 
 if options.prediction_type=="predict_genes":
     print ("Predicting drivers for %s (%s cell lines)."%(options.drug, dfc.shape[0]))
     dfimp = feature_importance(dfc)
-    dfimp.to_csv("%s/%s_imp.csv"%(options.output, options.drug.split("/")[0]), index=False)
-
-elif options.prediction_type=="feature_pruning":
-    print("Predicting drivers (data driven) for %s (%s cell lines)."%(options.drug, dff.shape[0]))
-    dfimp = feature_pruning(dff, options.num_features)
-    dfimp.to_csv("%s/%s_ddfs.csv"%(options.output, options.drug.split("/")[0]), index=False)
+    dfimp.to_csv("%s/%s_imp.csv"%(options.output, options.drug), index=False)
 
 elif options.prediction_type=="estimate_accuracy":
     msg = "Estimating accuracy of Random Forest model for %s (%s cell lines) from %s genes"
     print (msg%(options.drug, dfc.shape[0], dfc.shape[1]))
-    auc, dfout = compute_auc(dfc)
-    dfout.to_csv("%s/%s.csv"%(options.output, options.drug.split("/")[0]), index=False)
-    with open("%s/%s_auc.txt"%(options.output, options.drug.split("/")[0]),"w") as outFile:
+    auc, dfout = auc_random_forest(dfc)
+    dfout.to_csv("%s/%s_rfr.csv"%(options.output, options.drug), index=False)
+    with open("%s/%s_rfr.txt"%(options.output, options.drug),"w") as outFile:
         outFile.write(str(auc))
     outFile.close()
-
+else:
+    print("Invalid prediction type")
